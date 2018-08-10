97694dba09e6be2c4dc1f638029b07816d094e75
Scott Saloom <scottsaloom@gmail.com>
2018-08-10 11:37:21 -0500

    Updated Files

--- keys.js
+++ keys.js
@@ -3,10 +3,13 @@ exports.spotify = {
     secret: process.env.SPOTIFY_SECRET
 };
 
-exports.owm = {
-    id: process.env.OWM_ID
-}
+exports.twitter = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
 
 exports.omdb = {
     id: process.env.OMDB_ID
-}
+};
